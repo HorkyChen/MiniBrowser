bd8b35b3034f3398414cf2acad16d0e14d67df36
HorkyChen <horky.chen@gmail.com>
2013-04-01 15:28:55 +0800

    Initial Commit

---
+++ MiniBrowser/MiniBrowserDocument.h
@@ -0,0 +1,13 @@
+//
+//  MiniBrowserDocument.h
+//  MiniBrowser
+//
+//  Created by Horky Chen on 4/1/13.
+//  Copyright (c) 2013 Horky Chen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MiniBrowserDocument : NSDocument
+
+@end
---
+++ MiniBrowser/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MiniBrowser
+//
+//  Created by Horky Chen on 4/1/13.
+//  Copyright (c) 2013 Horky Chen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
